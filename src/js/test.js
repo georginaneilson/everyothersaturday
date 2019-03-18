09fb2848131d5be5cc0896b5857b1e2961ab1d18
Georgina Neilson <georgina.neilson@qnrl.com>
2019-03-18 17:14:10 +0000

    Add time and current day

--- src/js/test.js
+++ src/js/test.js
@@ -1,3 +1,4 @@
+console.log('ready');
 let today = new Date();
 let oneDay = 1000 * 60 * 60 * 24;
 let gamesAfterToday = [];
@@ -53,6 +54,11 @@ let fixtures = [
       home: 'Ibrox',
 },
 {
+  date: new Date("2019-03-18T12:00:00-07:30"),
+  team: 'Test',
+  home: 'Ibrox',
+},
+{
   date: new Date(today.getFullYear(), 2, 31),
     team: 'The Unwashed',
       home: 'away',
@@ -67,30 +73,61 @@ let fixtures = [
     team: 'Motherwell',
       home: 'away',
 },
+
 ]
 
 for (let i = 0; i < fixtures.length; i++) {
-  if (fixtures[i].date > today) {
+  console.log(fixtures[i]);
+  if (fixtures[i].date >= (today)) {
     gamesAfterToday.push(fixtures[i]);
+    console.log('today: ', today)
+     console.log('gamesAfterToday: ', gamesAfterToday);
   }
 }
 
 let closestMatch = gamesAfterToday[0];
+console.log('CM: ', closestMatch);
 let closestMatchDate = gamesAfterToday[0].date;
-let daysLeft = Math.ceil((closestMatchDate.getTime() - today.getTime()) / oneDay);
+let closestMatchDateDay = closestMatchDate.getDate();
+let todaysDay = today.getDate();
+
+let hours = closestMatchDate.getHours();
+let mins = closestMatchDate.getMinutes();
+let tMins = today.getMinutes();
+// if minutes are less than 10 we need to add a 0
+if(tMins < 10){
+  tMins = '' + 0 + tMins;
+}
+
+let todaysTime = '' + today.getHours() + tMins;
+
+let matchTime = '' + closestMatchDate.getHours() + closestMatchDate.getMinutes();
+let afterMatchTime = '' + (closestMatchDate.getHours() + 04) + closestMatchDate.getMinutes();
 let team = closestMatch.team;
 let home = closestMatch.home;
 
+if((todaysDay === closestMatchDateDay) && (todaysTime < afterMatchTime)){
+  console.log('playing today');
+  document.getElementById('optionalDate').remove();
+    document.getElementById('optionalDays').remove();
+    document.getElementById('optionalToday').innerHTML = " today";
+
+} else{
+  let daysLeft = Math.ceil((closestMatchDate.getTime() - today.getTime()) / oneDay);
+
 document.getElementById('date').innerHTML = daysLeft;
+console.log('daysleft: ', daysLeft);
+}
+document.getElementById('time').innerHTML = matchTime;
 document.getElementById('team').innerHTML = team;
 document.getElementById('home').innerHTML = home;
 
 let link = document.getElementById("stylesheet");
 
-if (closestMatch.home === "away") {
-  console.log("away");
-  link.setAttribute("href", "src/css/away.css");
-} else {
-  console.log("home");
-  link.setAttribute("href", "src/css/home.css");
-}
+// if (closestMatch.home === "away") {
+//   console.log("away");
+//   link.setAttribute("href", "src/css/away.css");
+// } else {
+//   console.log("home");
+//   link.setAttribute("href", "src/css/home.css");
+// }
